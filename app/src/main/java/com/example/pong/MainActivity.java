0d21247a3a3d18b62e3e705fcedc635cbd7f29a8
DONGGEUN JUNG <topsan72@gmail.com>
2023-01-11 16:52:52 -0500

    Add edge color

--- app/src/main/java/com/example/pong/MainActivity.java
+++ app/src/main/java/com/example/pong/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements JGameLib.GameEvent {
     JGameLib gameLib = null;
-    int m = 34, n = 20;
+    static int rows = 34, cols = 20;
     JGameLib.Card cardEdgeL;
     JGameLib.Card cardEdgeR;
     JGameLib.Card cardEdgeT;
@@ -35,36 +35,36 @@ public class MainActivity extends AppCompatActivity implements JGameLib.GameEven
     }
 
     private void initGame() {
-        gameLib.setScreenGrid(n,m);
+        gameLib.setScreenGrid(cols, rows);
         gameLib.listener(this);
         gameLib.addCardColor(Color.rgb(10,10,100));
         cardBall = gameLib.addCardColor(Color.rgb(255,255,255), 1,10,1,1);
         cardBall.checkCollision();
-        cardEdgeL = gameLib.addCardColor(Color.rgb(255,255,255), 0,0,1,m);
+        cardEdgeL = gameLib.addCardColor(Color.rgb(255,255,255), 0,0,1,rows);
         cardEdgeL.checkCollision();
-        cardEdgeR = gameLib.addCardColor(Color.rgb(255,255,255), n-1,0,1,m);
+        cardEdgeR = gameLib.addCardColor(Color.rgb(255,255,255), cols-1,0,1,rows);
         cardEdgeR.checkCollision();
-        cardEdgeT = gameLib.addCardColor(Color.rgb(255,255,140), 1,0,n-2,1);
+        cardEdgeT = gameLib.addCardColor(Color.rgb(255,255,140), 1,0,cols-2,1);
         cardEdgeT.checkCollision();
-        cardEdgeB = gameLib.addCardColor(Color.rgb(255,255,140), 1,m-1,n-2,1);
+        cardEdgeB = gameLib.addCardColor(Color.rgb(255,255,140), 1,rows-1,cols-2,1);
         cardEdgeB.checkCollision();
         cardRacket1 = gameLib.addCardColor(Color.rgb(255,255,255), 8,2,4,1);
         cardRacket1.checkCollision();
-        cardRacket2 = gameLib.addCardColor(Color.rgb(255,255,255), 8,m-3,4,1);
+        cardRacket2 = gameLib.addCardColor(Color.rgb(255,255,255), 8,rows-3,4,1);
         cardRacket2.checkCollision();
-        cardScore1 = gameLib.addCardColor(Color.argb(0,0,0,0), 1,13,4,4);
+        cardScore1 = gameLib.addCardColor(Color.TRANSPARENT, 1,13,4,4);
         cardScore1.text("0", Color.rgb(128,128,128), 100);
-        cardScore2 = gameLib.addCardColor(Color.argb(0,0,0,0), 1,17,4,4);
+        cardScore2 = gameLib.addCardColor(Color.TRANSPARENT, 1,17,4,4);
         cardScore2.text("0", Color.rgb(128,128,128), 100);
     }
 
     void newBall() {
         cardScore1.text("" + scoreCom);
         cardScore2.text("" + scoreMe);
-        int x = gameLib.random(1, n-2);
+        int x = gameLib.random(1, cols-2);
         cardBall.move(x,10);
-        double speed = 0.4;
-        if(gameLib.random(0,1) == 0)
+        double speed = 0.5;
+        if(gameLib.random(1) == 0)
             cardBall.movingEndless(speed, speed);
         else
             cardBall.movingEndless(-speed, speed);
@@ -90,14 +90,14 @@ public class MainActivity extends AppCompatActivity implements JGameLib.GameEven
         switch (v.getId()) {
             case R.id.btnLeft: {
                 if(cardRacket2.screenRect().left < 1+gapHrz)
-                    cardRacket2.move(1, m-3);
+                    cardRacket2.move(1, rows-3);
                 else
                     cardRacket2.moveGap(-gapHrz, 0);
                 break;
             }
             default: {
-                if(cardRacket2.screenRect().left > 15-gapHrz)
-                    cardRacket2.move(15, m-3);
+                if(cardRacket2.screenRect().right >= cols-1)
+                    cardRacket2.move(cols-cardRacket2.screenRect().width()-1, rows-3);
                 else
                     cardRacket2.moveGap(gapHrz, 0);
             }
@@ -130,7 +130,7 @@ public class MainActivity extends AppCompatActivity implements JGameLib.GameEven
             if(cardEdgeL.equals(card2))
                 cardBall.move(1, cardBall.screenRect().top);
             else
-                cardBall.move(n-2, cardBall.screenRect().top);
+                cardBall.move(cols-2, cardBall.screenRect().top);
             cardBall.movingEndless(-cardBall.unitHrz, cardBall.unitVtc);
         } else if (cardRacket1.equals(card2) || cardRacket2.equals(card2)) {
             cardBall.movingEndless(cardBall.unitHrz, -cardBall.unitVtc);
